855f9358a82b54a3c3e68898a49f6285fb5487b8
Braydon Lowe <braydonslowe17@gmail.com>
2024-12-02 15:10:03 -0700

    Order Router finished

--- src/routes/orderRouter.test.js
+++ src/routes/orderRouter.test.js
@@ -2,6 +2,9 @@ const app = require('../service');
 const request = require('supertest');
 const { createAdminUser } = require('../utils/createAdminUser');
 const { createTestUser } = require('../utils/createUser');
+const { createFranchiseAndStore } = require('../utils/createFranchiseAndStore');
+const randomName = require('../utils/randomName');
+require('jest-fetch-mock').enableMocks();
 
 let adminAuth;
 let admin;
@@ -58,9 +61,75 @@ test('getMenu', async () => {
 
 
 test('CreateOrder', async () => {
-    const orderData = {
-        franchiseId: 1,
-        storeId: 1,
-        items: [{}]
-    }
+    fetch.mockResponseOnce(JSON.stringify({ message: 'Success' }), { status: 200 });
+
+    const [franchiseResponse, storeResponse] = await createFranchiseAndStore(adminAuth, admin);
+    const franchID = franchiseResponse.body.id;
+    const storeID = storeResponse.body.id;
+    
+    const createRepsonse = await request(app)
+        .put('/api/order/menu')
+        .set('Authorization', `Bearer ${adminAuth}`)
+        .send(item);
+
+    const itemID = createRepsonse.body[createRepsonse.body.length - 1].id;
+    const description1 = createRepsonse.body[createRepsonse.body.length - 1].description;
+    const price1 = createRepsonse.body[createRepsonse.body.length - 1].price;
+    const order = {
+        franchiseId: franchID,
+        storeId: storeID,
+        items: [{ 
+            menuId: itemID, 
+            description: description1,
+            price: price1
+        }]
+    };
+    const user = await createTestUser();
+    const userAuth = user.token;
+
+    const response = await request(app)
+        .post('/api/order')
+        .set('Authorization', `Bearer ${userAuth}`)
+        .send(order);
+
+    expect(response.status).toBe(200);
+
+});
+
+
+test('CreateOrder 500 Error', async () => {
+    fetch.mockResponseOnce(JSON.stringify({ message: 'Factory error' }), { status: 500 });
+
+    const [franchiseResponse, storeResponse] = await createFranchiseAndStore(adminAuth, admin);
+    const franchID = franchiseResponse.body.id;
+    const storeID = storeResponse.body.id;
+    
+    const createRepsonse = await request(app)
+        .put('/api/order/menu')
+        .set('Authorization', `Bearer ${adminAuth}`)
+        .send(item);
+
+    const itemID = createRepsonse.body[createRepsonse.body.length - 1].id;
+    const description1 = createRepsonse.body[createRepsonse.body.length - 1].description;
+    const price1 = createRepsonse.body[createRepsonse.body.length - 1].price;
+    //storeId changed to stireId
+    const order = {
+        franchiseId: franchID,
+        storeId: storeID,
+        items: [{ 
+            menuId: itemID, 
+            description: description1,
+            price: price1
+        }]
+    };
+    const user = await createTestUser();
+    const userAuth = user.token;
+
+    const response = await request(app)
+        .post('/api/order')
+        .set('Authorization', `Bearer ${userAuth}`)
+        .send(order);
+
+    expect(response.status).toBe(500);
+
 })
